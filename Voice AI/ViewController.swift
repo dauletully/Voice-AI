6effc5c8a8da5bdd95e35e9ed0acebfd02edca04
Nurlybakyt <begaly606@gmail.com>
2024-03-21 14:05:10 +0500

    Initial Commit

---
+++ Voice AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Voice AI
+//
+//  Created by Dinmukhammed Begaly on 21.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
